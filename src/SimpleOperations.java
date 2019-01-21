9624c8fe2bd7db099a32a438adc85a40d27b226c
LIEU Thierry <thierry.lieu@sap.com>
2019-01-21 13:33:46 +0000

    Implemented Substraction

--- src/SimpleOperations.java
+++ src/SimpleOperations.java
@@ -3,7 +3,10 @@ public class SimpleOperation {
 	// Here should be added the method addition
 
 	// Here should be added the method subtraction
-
+	public int substraction(int a, int b) {
+		return a-b;
+	} 
+	
 	// Here should be added the method multiplication
 	
 	// Here should be added the method division
