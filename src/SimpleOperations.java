754fd5e07533ae5c1946ba06df8216823f158413
LIEU Thierry <thierry.lieu@sap.com>
2019-01-21 13:03:31 +0000

    Creation of the class SimpleOperation

---
+++ src/SimpleOperations.java
@@ -0,0 +1,12 @@
+public class SimpleOperation {
+
+	// Here should be added the method addition
+
+	// Here should be added the method subtraction
+
+	// Here should be added the method multiplication
+	
+	// Here should be added the method division
+
+	
+}
